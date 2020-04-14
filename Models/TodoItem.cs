7381c41cfaaf38c811f7105c6cd13eeb6e144a12
DariaKrysiak <daria.krysiak@gmail.com>
2020-04-14 15:24:21 +0200

    Remove unused usings

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TutorialApi.Models
+﻿namespace TutorialApi.Models
 {
     public class TodoItem
     {
