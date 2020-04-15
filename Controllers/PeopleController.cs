e8fd27e2a18f9900c599479405cd3cd11b62e0bd
DariaKrysiak <daria.krysiak@gmail.com>
2020-04-15 19:46:40 +0200

    Add PeopleController

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TutorialApi.Models;
+
+namespace TutorialApi.Controllers
+{
+    [Route("api/people")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public PeopleController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Person>>> GetPeople()
+        {
+            return await _context.People.ToListAsync();
+        }
+    }
+}
